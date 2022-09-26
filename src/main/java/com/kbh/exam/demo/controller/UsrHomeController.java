7b5cde7d5a53dde0aced393eb1ee30b3e992ba15
byeonghunki <orangeys1000@gmail.com>
2022-09-26 16:18:08 +0900

    ArticleContoller 게시물 추가 구현

--- src/main/java/com/kbh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kbh/exam/demo/controller/UsrHomeController.java
@@ -9,62 +9,80 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.kbh.exam.demo.vo.Article;
 
 @Controller
 public class UsrHomeController {
-	@RequestMapping("/usr/home/getString")
+	@RequestMapping("usr/home/getString")
 	@ResponseBody
 	public String getString() {
 		return "hi";
 	}
 
-	@RequestMapping("/usr/home/getInt")
+	@RequestMapping("usr/home/getInt")
 	@ResponseBody
 	public int getInt() {
 		return 10;
 	}
 
-	@RequestMapping("/usr/home/getFloat")
+	@RequestMapping("usr/home/getFloat")
 	@ResponseBody
 	public float getFloat() {
 		return 10.5f;
 	}
 
-	@RequestMapping("/usr/home/getBoolean")
+	@RequestMapping("usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+
+	@RequestMapping("usr/home/getBoolean")
 	@ResponseBody
 	public boolean getBoolean() {
 		return true;
 	}
 
-	@RequestMapping("/usr/home/getChar")
+	@RequestMapping("usr/home/getChar")
 	@ResponseBody
 	public char getChar() {
 		return 'a';
 	}
-	@RequestMapping("/usr/home/getMap")
+
+	@RequestMapping("usr/home/getMap")
 	@ResponseBody
-	public Map<String,Object> getMap() {
-		Map<String,Object> map = new HashMap<>();
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
 		map.put("철수나이", 22);
 		map.put("영희나이", 33);
+
 		return map;
 	}
+
 	@RequestMapping("usr/home/getList")
 	@ResponseBody
 	public List<String> getList() {
 		List<String> list = new ArrayList<>();
 		list.add("철수나이");
 		list.add("영희나이");
+
 		return list;
 	}
+
+	@RequestMapping("usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1", "내용1");
+		Article article2 = new Article(2, "제목2", "내용2");
+
+		return article;
+	}
+
 	@RequestMapping("usr/home/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1");
-		Article article2 = new Article(2, "제목2");
+		Article article1 = new Article(1, "제목1", "내용1");
+		Article article2 = new Article(2, "제목2", "내용2");
 
 		List<Article> articles = new ArrayList<>();
 		articles.add(article1);
@@ -72,13 +90,4 @@ public class UsrHomeController {
 
 		return articles;
 	}
-}
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class Article {
-	private int id;
-	private String title;
-	
 }

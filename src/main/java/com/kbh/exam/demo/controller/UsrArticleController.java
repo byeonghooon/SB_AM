985a8bdcf358e13fd96f9e74bd70f5696901675b
byeonghunki <orangeys1000@gmail.com>
2022-09-27 10:46:46 +0900

    리스트 구현

--- src/main/java/com/kbh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kbh/exam/demo/controller/UsrArticleController.java
@@ -25,11 +25,18 @@ public class UsrArticleController {
 	public Article doAdd(String title, String body) {
 		int id = lastArticleId + 1;
 		Article article = new Article(id, title, body);
-		
+
 		articles.add(article);
 		lastArticleId = id;
 
 		return article;
+	}
+
+	@RequestMapping("usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+
+		return articles;
 	}
 
 }

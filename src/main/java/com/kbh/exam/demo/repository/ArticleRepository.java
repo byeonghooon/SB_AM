601f225a40e4218b000d447f2d3cc68e89ef5857
byeonghunki <orangeys1000@gmail.com>
2022-09-28 11:50:00 +0900

    INSERT 쿼리 실행되도록 return 타입 수정, last_insert_id()

--- src/main/java/com/kbh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kbh/exam/demo/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.kbh.exam.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -13,7 +14,8 @@ import com.kbh.exam.demo.vo.Article;
 public interface ArticleRepository {
 	
 	//INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?;
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(String title, String body);
 		
 	//SELECT * FROM article WHERE id = ?;
 	@Select("SELECT * FROM article WHERE id = #{id}")
@@ -27,8 +29,11 @@ public interface ArticleRepository {
 	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(int id);
 
-	//UPDATE article SET title = ?, `body` = ?, updateDate = NOW(), WHERE id = ?;
+	//UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
 	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
+	
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 
 }

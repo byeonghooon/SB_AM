5150c8a21de13ab1fb075821e8b51a0bea0720e8
byeonghunki <orangeys1000@gmail.com>
2022-09-29 13:58:47 +0900

    회원가입 준비, UsrMemberController,MemberService 추가

--- src/main/java/com/kbh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kbh/exam/demo/repository/ArticleRepository.java
@@ -2,11 +2,7 @@ package com.kbh.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.kbh.exam.demo.vo.Article;
 
---
+++ src/main/java/com/kbh/exam/demo/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.kbh.exam.demo.service;
+
+public class MemberService {
+
+//	private MemberRepository memberRepository;
+
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+//		memberRepository.join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+
+	}
+}

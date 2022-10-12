32c5849657d259e52d1770a223f5dd4aac7ac80c
byeonghunki <orangeys1000@gmail.com>
2022-10-12 12:28:11 +0900

    Rq 객체를 BeforeActionInterceptor 에서 한번만 생성

--- src/main/java/com/kbh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kbh/exam/demo/controller/UsrArticleController.java
@@ -27,7 +27,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (rq.isLogined == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요");
@@ -51,7 +51,7 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/list")
 	public String showList(HttpServletRequest req, Model model) {
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 
 		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
 
@@ -64,8 +64,8 @@ public class UsrArticleController {
 	@ResponseBody
 	public String doDelete(HttpServletRequest req, int id) {
 
-		Rq rq = new Rq(req);
-
+		Rq rq = (Rq) req.getAttribute("rq");
+		
 		if (rq.isLogined == false) {
 			return Ut.jsHistoryBack("로그인 후 이용해주세요");
 		}
@@ -89,7 +89,9 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData doModify(HttpServletRequest req, int id, String title, String body) {
-		Rq rq = new Rq(req);
+
+		Rq rq = (Rq) req.getAttribute("rq");
+		
 		if (rq.isLogined == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요");
 		}
@@ -112,8 +114,9 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(HttpServletRequest req, Model model, int id) {
-		Rq rq = new Rq(req);
 
+		Rq rq = (Rq) req.getAttribute("rq");
+		
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		model.addAttribute("article", article);

67c6920e10c55f54588173b48bbc6414573dc98a
byeonghunki <orangeys1000@gmail.com>
2022-10-19 20:38:49 +0900

    페이지 메뉴의 크기를 게시글의 갯수에 맞게 동적으로 조절

--- src/main/java/com/kbh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kbh/exam/demo/controller/UsrArticleController.java
@@ -69,14 +69,16 @@ public class UsrArticleController {
 
 		int itemsInAPage = 10;
 		
-		int totalPage = (int) Math.ceil((double) articlesCount / itemsInAPage);
+		int pagesCount = (int) Math.ceil((double) articlesCount / itemsInAPage);
 
 		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, page,
 				itemsInAPage);
 
+		model.addAttribute("boardId", boardId);
 		model.addAttribute("board", board);
 		model.addAttribute("articlesCount", articlesCount);
-		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("page", page);
+		model.addAttribute("pagesCount", pagesCount);
 		model.addAttribute("articles", articles);
 
 		return "usr/article/list";

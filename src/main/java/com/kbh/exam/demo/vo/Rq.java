190570ad453155cedd976b1a5f339fce9f190514
byeonghunki <orangeys1000@gmail.com>
2022-11-13 10:59:06 +0900

    회원가입 폼 제작

--- src/main/java/com/kbh/exam/demo/vo/Rq.java
+++ src/main/java/com/kbh/exam/demo/vo/Rq.java
@@ -94,6 +94,17 @@ public class Rq {
 		return Ut.jsHistoryBack(msg);
 	}
 
+	public String jsHistoryBackOnView(String resultCode, String msg) {
+		req.setAttribute("msg", String.format("[%s] %s", resultCode, msg));
+		req.setAttribute("historyBack", true);
+		return "usr/common/js";
+	}
+
+	public String jsHistoryBack(String resultCode, String msg) {
+		msg = String.format("[%s] %s", resultCode, msg);
+		return Ut.jsHistoryBack(msg);
+	}
+
 	public String jsReplace(String msg, String uri) {
 		return Ut.jsReplace(msg, uri);
 	}
@@ -118,6 +129,10 @@ public class Rq {
 		resp.setContentType("text/html; charset=UTF-8");
 
 		print(Ut.jsReplace(msg, url));
+	}
+
+	public String getJoinUri() {
+		return "../member/join?afterLoginUri=" + getAfterLoginUri();
 	}
 
 	public String getLoginUri() {

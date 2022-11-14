5bd00f2f6e35bdeb94c98eee037ccb5b4693388c
byeonghunki <orangeys1000@gmail.com>
2022-11-14 14:53:07 +0900

    AJAX 추가

--- src/main/java/com/kbh/exam/demo/controller/UsrAjaxTextController.java
+++ src/main/java/com/kbh/exam/demo/controller/UsrAjaxTextController.java
@@ -1,22 +1,34 @@
 package com.kbh.exam.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrAjaxTextController {
-	
-	@RequestMapping("usr/home/plus")
-	String showTestPage() {
-		return "usr/home/plus";
-	}
 
 	@RequestMapping("usr/home/doPlus")
 	@ResponseBody
-	int doPlus(int num1, int num2) {
-		return num1 + num2;
+	String doPlus(int num1, int num2) {
+		String msg = "더하기 성공!";
+		int rs = num1 + num2;
+
+		return rs + "/" + msg + "/S-1";
 	}
 
+	@RequestMapping("usr/home/doPlusJson")
+	@ResponseBody
+	Map doPlusJson(int num1, int num2) {
+		Map rs = new HashMap<String, Object>();
+
+		rs.put("rs", num1 + num2);
+		rs.put("msg", "더하기 성공!");
+		rs.put("code", "S-1");
+
+		return rs;
+	}
 
 }

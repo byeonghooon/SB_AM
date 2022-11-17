0b94e733d75b60fc97ffe515634668dee9163a6e
byeonghunki <orangeys1000@gmail.com>
2022-11-17 14:31:09 +0900

    요청이 ajax인지 아닌지 판별, 인터셉터에서 올바른 형식으로 응답, 회원가입을 하는 도중 다른 탭에서 로그인을 했다면 진행…

--- src/main/java/com/kbh/exam/demo/interceptor/NeedLogoutInterceptor.java
+++ src/main/java/com/kbh/exam/demo/interceptor/NeedLogoutInterceptor.java
@@ -13,17 +13,19 @@ import com.kbh.exam.demo.vo.Rq;
 public class NeedLogoutInterceptor implements HandlerInterceptor {
 	@Autowired
 	private Rq rq;
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
 
 		if (rq.isLogined()) {
-
-			rq.printHistoryBackJs("로그아웃 후 이용해주세요");
-
+			if (rq.isAjax()) {
+				resp.setContentType("application/json; charset=UTF-8");
+				resp.getWriter().append("{\"resultCode\":\"F-B\",\"msg\":\"로그아웃 후 이용해주세요\"}");
+			} else {
+				rq.printHistoryBackJs("로그아웃 후 이용해주세요");
+			}
 			return false;
 		}
-
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
